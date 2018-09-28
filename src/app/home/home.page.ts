2d0bc39ac850d35feba45a93ca2c4b25fcab32e3
Ewerton Madruga <emadruga@gmail.com>
2018-09-28 17:43:15 -0300

    - copy files from opener folder of an existing prototype into home.page files. - add ionic storage with sqlite plugin, just in case we need it.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  constructor(private navCtrl: NavController) { }
+
+  ngOnInit() {
+  }
+
+    doForm() {
+	console.log("Vamos criar nova ficha de pré-inscrição");
+	this.navCtrl.navigateForward('/NovoCadastro');
+    }
+
+    doLogin() {
+	console.log("Vamos atualizar dados de pré-inscrição de candidato existente");
+	this.navCtrl.navigateForward('/FazerLogin');
+    }
 
 }
