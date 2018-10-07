a82cf7a49f8030ec2e55185306e8e8ccad88dab3
Ewerton Madruga <emadruga@gmail.com>
2018-10-07 17:58:05 -0300

    - pointing prod to inmetro.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,4 @@ export const environment = {
     production: true,
     message: 'ok'
 };
-export const SERVER_URL = 'https://shielded-temple-91138.herokuapp.com';
+export const SERVER_URL = 'https://cibernetica.inmetro.gov.br';
